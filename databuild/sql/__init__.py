9078cb5e66a8c600fa603b9728791825251e37e7
Yuan Zhang <zhangyuan@users.noreply.github.com>
2023-02-24 12:33:19 +0800

    compile template from directory (#7)
    * compile file from directory
    
    * get rid of useless Script

--- databuild/sql/__init__.py
+++ databuild/sql/__init__.py
@@ -2,17 +2,13 @@ import jinja2
 
 
 class Context:
-    def __init__(self, params: dict) -> None:
+    def __init__(self, params: dict, template_directory: str = None) -> None:
         self.params = params
+        self.template_directory = template_directory
 
 
 class Macro:
     pass
-
-
-class Script:
-    def __init__(self, content: str) -> None:
-        self.content = content
 
 
 class Compiler:
@@ -20,13 +16,27 @@ class Compiler:
         j2_template = jinja2.Template(template, undefined=jinja2.StrictUndefined)
         return j2_template.render(params=params)
 
+    def compile_file(self, template_directory, template_name, params):
+        loader = jinja2.FileSystemLoader(template_directory)
+        environment = jinja2.Environment(loader=loader)
+
+        template = environment.get_template(template_name)
+        return template.render(params=params)
+
 
 class SparkRunner:
     def __init__(self, spark_session, compiler=None) -> None:
         self.compiler = compiler if compiler is not None else Compiler()
         self.spark_session = spark_session
 
-    def run(self, script: Script, context: Context = None):
+    def execute_template(self, template: str, context: Context = None):
         params = context.params if context else {}
-        statement = self.compiler.compile(script.content, params=params)
+        statement = self.compiler.compile(template, params=params)
+        return self.spark_session.sql(statement)
+
+    def execute_template_path(self, template_path, context: Context = None):
+        params = context.params if context else {}
+        statement = self.compiler.compile_file(
+            context.template_directory, template_path, params=params
+        )
         return self.spark_session.sql(statement)
--- databuild/test/test_sql.py
+++ databuild/test/test_sql.py
@@ -1,4 +1,5 @@
-from sql import Script, SparkRunner, Context
+from pathlib import Path
+from sql import SparkRunner, Context
 from spark import get_or_create_local_session
 from spark.testing import assert_dataframes
 
@@ -8,10 +9,10 @@ class TestSQL:
         spark_session = get_or_create_local_session("app")
         runner = SparkRunner(spark_session=spark_session)
 
-        script = Script("select 1")
+        statement = "select 1"
 
         # pylint: disable=invalid-name
-        df = runner.run(script)
+        df = runner.execute_template(statement)
 
         expected_df = spark_session.sql("select 1")
         assert_dataframes(expected_df, df)
@@ -20,14 +21,31 @@ class TestSQL:
         spark_session = get_or_create_local_session("app")
         runner = SparkRunner(spark_session=spark_session)
 
-        context = Context(params={"logical_date": "2023-01-01"})
+        context = Context(params={"logical_date": "2023-01-01"}, template_directory=".")
 
         statement = "SELECT '{{ params.logical_date }}' AS date"
 
-        script = Script(statement)
+        # pylint: disable=invalid-name
+        df = runner.execute_template(statement, context=context)
+
+        expected_df = spark_session.sql("select '2023-01-01' AS date")
+        assert_dataframes(expected_df, df)
+
+    def test_compile_template_file(self):
+        spark_session = get_or_create_local_session("app")
+        runner = SparkRunner(spark_session=spark_session)
+
+        path = Path(__file__)
+        template_directory = path.joinpath(
+            path.parent.absolute(), "fixtures", "templates"
+        )
+
+        context = Context(
+            params={"logical_date": "2023-01-01"}, template_directory=template_directory
+        )
 
         # pylint: disable=invalid-name
-        df = runner.run(script, context=context)
+        df = runner.execute_template_path("select_date.j2.sql", context=context)
 
         expected_df = spark_session.sql("select '2023-01-01' AS date")
         assert_dataframes(expected_df, df)
