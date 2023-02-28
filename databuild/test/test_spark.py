d40612a8921388910022fa10f2a9b10e4937bcb2
Yuan Zhang <zhangyuan@users.noreply.github.com>
2023-03-01 00:40:42 +0800

    add test to assert csv with multiple rows

--- databuild/test/test_spark.py
+++ databuild/test/test_spark.py
@@ -22,6 +22,25 @@ class TestSpark:
         )
         assert_dataframes(expected_df, df)
 
+    def test_load_csv_with_multiple_rows_as_table(self):
+        spark_session = get_or_create_local_session("app")
+        load_table_from_csv(
+            spark_session,
+            "orders",
+            test_helper.fixture_path("fixtures/data/orders_multiple.csv"),
+        )
+        df = spark_session.sql("select * from orders")
+        expected_df = spark_session.sql(
+            """
+        SELECT data.*
+            FROM VALUES
+                    ('ODER12345', 10000, DATE('2023-01-01 18:00:00')),
+                    ('ODER12346', 10000, DATE('2023-01-01 19:00:00'))
+                AS data(id, user_id, created_at)
+        """
+        )
+        assert_dataframes(expected_df, df)        
+
     def test_assert_csv_and_table(self):
         spark_session = get_or_create_local_session("app")
         spark_session.sql(
