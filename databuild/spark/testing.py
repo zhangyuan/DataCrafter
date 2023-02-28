1de889d9e205d82bdf6e973fce13d7c4a1ab88a7
Yuan Zhang <zhangyuan@users.noreply.github.com>
2023-03-01 00:37:31 +0800

    assert csv with table

--- databuild/spark/testing.py
+++ databuild/spark/testing.py
@@ -1,2 +1,15 @@
+from spark import helpers
+from pyspark.sql import SparkSession
+
+
 def assert_dataframes(df1, df2):
     assert df1.collect() == df2.collect()
+
+
+def assert_csv_with_table(spark_session: SparkSession, csv_path, table_name):
+    helpers.load_table_from_csv(
+        spark_session=spark_session, table_name="expected_table", file_path=csv_path
+    )
+    expected_df = spark_session.sql("select * from expected_table")
+    actual_df = spark_session.sql(f"select * from {table_name}")
+    assert_dataframes(expected_df, actual_df)
--- databuild/test/test_spark.py
+++ databuild/test/test_spark.py
@@ -1,6 +1,6 @@
 from test import test_helper
-from spark import get_or_create_local_session, load_table_from_csv
-from spark.testing import assert_dataframes
+from spark.helpers import get_or_create_local_session, load_table_from_csv
+from spark.testing import assert_dataframes, assert_csv_with_table
 
 
 class TestSpark:
@@ -21,3 +21,20 @@ class TestSpark:
         """
         )
         assert_dataframes(expected_df, df)
+
+    def test_assert_csv_and_table(self):
+        spark_session = get_or_create_local_session("app")
+        spark_session.sql(
+            """
+        SELECT data.*
+            FROM VALUES
+                    ('ODER12345', 10000, DATE('2023-01-01 18:00:00'))
+                AS data(id, user_id, created_at)
+        """
+        ).createOrReplaceTempView("orders")
+
+        assert_csv_with_table(
+            spark_session=spark_session,
+            csv_path=test_helper.fixture_path("fixtures/data/orders.csv"),
+            table_name="orders",
+        )
